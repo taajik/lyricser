25905f9a6ff866dc5953e43821c02c17c077a9a0
aaronn_t <75927372+aaronn44@users.noreply.github.com>
2021-08-10 16:12:30 +0430

    some refactoring and add search_lyrics function

--- add_lyrics.py
+++ add_lyrics.py
@@ -5,9 +5,9 @@ from mutagen.mp4 import MP4
 import lyricsgenius
 
 
-songs_path = "/" + input("Enter path of your musics folder: ").strip("/") + "/"
+songs_path = '/' + input("Enter path of your musics folder: ").strip().strip('\'').strip('/') + '/'
 
-if not os.path.isdir(songs_path) or songs_path == "//":
+if not (os.path.isdir(songs_path) and songs_path!="//" and songs_path!="/./"):
     songs_path = os.getcwd() + "/songs/"
     if not os.path.isdir(songs_path):
         os.mkdir(songs_path)
@@ -17,22 +17,21 @@ if not os.path.isdir(without_lyrics_path):
     os.mkdir(without_lyrics_path)
 
 files = sorted(os.listdir(songs_path))
-if not files or songs_path=="//" or not songs_path:
-    print("\n songs folder is empty!")
+if not files:
+    print("\n Songs folder is empty!")
     exit()
-
 
 def move_to_without_lyrics(file):
     mv = f"mv \"{file}\" \"{without_lyrics_path}\""
-    if os.path.isfile(file) and not input(f" CONFIRM: {mv} "):
+    if os.path.isfile(file) and not input(f"  CONFIRM: {mv} "):
         os.system(mv)
 
 added_lyrics = 0
 not_added_lyrics = 0
-def add(i, file=""):
+def add(b, file=''):
     global added_lyrics
     global not_added_lyrics
-    if i:
+    if b:
         added_lyrics += 1
     else:
         not_added_lyrics += 1
@@ -40,41 +39,38 @@ def add(i, file=""):
             move_to_without_lyrics(file)
 
 
-client_access_token = 'JPmYaQ8e0nffrGMNZB-S2-5qxjAlVEb7YNtrGUG58PyCmy4jG5Z9pK2BN6kPZ6kW'
+client_access_token = "JPmYaQ8e0nffrGMNZB-S2-5qxjAlVEb7YNtrGUG58PyCmy4jG5Z9pK2BN6kPZ6kW"
 genius = lyricsgenius.Genius(client_access_token, skip_non_songs=True, excluded_terms=["(Remix)", "(Live)"])
 # genius = lyricsgenius.Genius(client_access_token, skip_non_songs=True, excluded_terms=["(Live)"])
+# genius = lyricsgenius.Genius(client_access_token, skip_non_songs=True)
 
 def set_lyrics_mp3(file):
     try:
         tag = eyed3.load(file).tag
-    except:
-        print(" X file not found:", file)
-        add(False)
-        return
-
-    try:
         t = tag.title
         aa = tag.album_artist
         if '(' in t or ')' in t:
-            print(t, "-", aa)
+            print(t, '-', aa)
             t = input("  t: ")
             if not t.strip():
                 t = tag.title
-        lyrics = genius.search_song(t, aa, get_full_info=False).lyrics
-        # lyrics = genius.search_song(song_id=, get_full_info=False).lyrics
-        # lyrics = """"""
     except:
-        print(" X lyrics not found:", file)
+        print(" X File Error:", file)
         add(False, file)
         return
 
     try:
+        lyrics = genius.search_song(t, aa, get_full_info=False).lyrics
+        # print(t, '-', aa)
+        # lyrics = genius.lyrics(input("  url: "))
+        # lyrics = genius.search_song(song_id=input("  id: "), get_full_info=False).lyrics
+        # lyrics = """"""
         tag.lyrics.set(lyrics.strip())
         tag.save(version=eyed3.id3.ID3_V2_4, encoding="utf8")
         add(True)
         return
     except:
-        print(" X lyrics not added:", file)
+        print(" X Lyrics Error:", file)
         add(False, file)
         return
 
@@ -82,30 +78,32 @@ def set_lyrics_mp3(file):
 def set_lyrics_m4a(file):
     try:
         tags = MP4(file).tags
-        t = tags.get('\xa9nam')
-        aa = tags.get('aART')
+        t = tags.get("\xa9nam")
+        aa = tags.get("aART")
         if len(t) == 1 and len(aa) == 1:
             t = t[0]
             aa = aa[0]
         else:
-            print(t, "-", aa)
+            print(t, '-', aa)
             t = input("  t: ")
-            aa = input(" aa: ")
+            aa = input("  aa: ")
         if '(' in t or ')' in t:
-            print(t, "-", aa)
+            print(t, '-', aa)
             t = input("  t: ")
             if not t.strip():
-                t = tags.get('\xa9nam')[0]
+                t = tags.get("\xa9nam")[0]
     except:
         print(" X File Error:", file)
-        add(False)
+        add(False, file)
         return
 
     try:
         lyrics = genius.search_song(t, aa, get_full_info=False).lyrics
-        # lyrics = genius.search_song(song_id=, get_full_info=False).lyrics
+        # print(t, '-', aa)
+        # lyrics = genius.lyrics(input("  url: "))
+        # lyrics = genius.search_song(song_id=input("  id: "), get_full_info=False).lyrics
         # lyrics = """"""
-        tags['\xa9lyr'] = lyrics
+        tags["\xa9lyr"] = lyrics.strip()
         tags.save(file)
         add(True)
         return
@@ -116,9 +114,9 @@ def set_lyrics_m4a(file):
 
 
 def set_lyrics(path, files):
-    print("\n" + "─"*len(path) + "──┐")
-    print(" " + path + " │", len(files))
-    print("─"*len(path) + "──┘")
+    print('\n' + '─'*len(path) + '──┐')
+    print(' ' + path + ' │', len(files))
+    print('─'*len(path) + '──┘')
 
     files2 = []
     i = 0
@@ -129,9 +127,8 @@ def set_lyrics(path, files):
             print('\n', i, end=": ")
             if file[-4:].lower() == ".m4a":
                 set_lyrics_m4a(file)
-            else:
+            elif file[-4:].lower() == ".mp3":
                 set_lyrics_mp3(file)
-
         elif os.path.isdir(file):
             files2.append(file + '/')
 
@@ -139,60 +136,75 @@ def set_lyrics(path, files):
         set_lyrics(path2, sorted(os.listdir(path2)))
 
 
-def see_lyrics(path, files): # NOTE: needs a review or maybe just remove it all
-    print("\n" + "┌─" + "─"*len(path) + "─┐")
-    print("│ " + path + " │")
-    print("└─" + "─"*len(path) + "─┘")
-    print("\n" + "─"*os.get_terminal_size()[0])
+def show_lyrics(path, files, txt=True):
+    lyrics = ''
+    folder = path.split('/')[-2]
+    lyrics += f"\n┌─{'─'*len(folder)}─┐\n│ {folder} │ {len(files)}\n└─{'─'*len(folder)}─┘\n"
+    files2 = []
+    i = 0
+    for file in files:
+        file = path + file
+        if os.path.isfile(file):
+            i += 1
+            lyrics += f"\n\n{'─'*os.get_terminal_size()[0]}\n\n  {file[len(path):-4]}\n\n\n"
+
+            try:
+                if file[-4:].lower() == ".m4a":
+                    lyrics += MP4(file).tags.get("\xa9lyr")[0] + '\n'
+                elif file[-4:].lower() == ".mp3":
+                    lyrics += eyed3.load(file).tag.lyrics[0].text + '\n'
+            except:
+                lyrics += "    No Lyrics: " + file[len(path):] + '\n'
+
+        elif os.path.isdir(file):
+            files2.append(file + '/')
+    lyrics += f"\n\n{'─'*os.get_terminal_size()[0]}\n"
+    for path2 in files2: # try moving this for loop to the end of the function.
+        show_lyrics(path2, sorted(os.listdir(path2)), txt)
+
+    if txt:
+        lyricstxt = open("/home/leli/Music/Lyrics/"+folder+" (Lyrics).txt", 'w')
+        # lyricstxt = open(path+folder+" (Lyrics).txt", 'w')
+        lyricstxt.write(lyrics)
+        lyricstxt.close()
+        print("\n Lyrics file generated.")
+    else:
+        print(lyrics)
+
+
+def search_lyrics(path, files):
+    # print('\n', path, '\n')
     files2 = []
     for file in files:
         file = path + file
         if os.path.isfile(file):
             try:
-                print("\n\n  ", files.index(file[len(path):])+1, "/", len(files), sep="",  end = " ")
-                print(file[len(path):-4] + "\n\n")
                 if file[-4:].lower() == ".m4a":
-                    print(MP4(file).tags.get("\xa9lyr")[0])
-                else:
-                    print(eyed3.load(file).tag.lyrics[0].text)
+                    lyrics = MP4(file).tags.get("\xa9lyr")[0].lower()
+                elif file[-4:].lower() == ".mp3":
+                    lyrics = eyed3.load(file).tag.lyrics[0].text.lower()
+                if lyrics.strip() == '':
+                    raise Exception
+                if q != '' and q in lyrics:
+                    print('\n', file[len(path):])
+                    # print(lyrics[lyrics.index(q)-50:lyrics.index(q)+50])
             except:
-                print("    file not found:", file[len(path):])
-            print("\n\n" + "─"*os.get_terminal_size()[0])
-        elif os.path.isdir(file):
-            files2.append(file + '/')
-
-    for path2 in files2:
-        see_lyrics(path2, sorted(os.listdir(path2)))
-
-def find_without_lyrics(path, files):
-    print(path)
-    files2 = []
-    for file in files:
-        file = path + file
-        if os.path.isfile(file):
-            try:
-                if file[-4:].lower() == ".m4a":
-                    if not MP4(file).tags.get("\xa9lyr")[0]:
-                        raise Exception
-                else:
-                    if not eyed3.load(file).tag.lyrics[0].text.strip():
-                        raise Exception
-            except:
-                print(file[len(path):])
+                print('\n', " X No Lyrics:", file[len(path):], '\n')
         elif os.path.isdir(file):
             files2.append(file + '/')
     for path2 in files2:
-        find_without_lyrics(path2, sorted(os.listdir(path2)))
+        search_lyrics(path2, sorted(os.listdir(path2)))
 
-sss = input(" 1.Set lyrics\n 2.See lyrics\n:")
-print()
+
+sss = input(" 1.Set lyrics\n 2.Show lyrics\n 3.Search lyrics\n:")
 if sss == '1':
-    print('\n', songs_path)
-    if not input(" SET LYRICS? ").split():
-        set_lyrics(songs_path, files)
-        print(f"\n\n\n successfulls: {added_lyrics} \n unsuccessfulls: {not_added_lyrics}")
+    set_lyrics(songs_path, files)
+    print(f"\n\n\n successfulls: {added_lyrics} \n unsuccessfulls: {not_added_lyrics}")
 elif sss == '2':
-    see_lyrics(songs_path, files)
+    show_lyrics(songs_path, files)
 elif sss == '3':
-    find_without_lyrics(songs_path, files)
+    q = input(" q: ").lower()
+    # if len(q) < 4:
+    #     q = ''
+    search_lyrics(songs_path, files)
 print()
