f85245370da9d6641455dc1561f91ed832145d17
aaronn_t <75927372+aaronn44@users.noreply.github.com>
2021-08-11 13:27:12 +0430

    add comments (and minor changes)

--- add_lyrics.py
+++ add_lyrics.py
@@ -7,10 +7,13 @@ import lyricsgenius
 
 
 
+## number of
 added_lyrics = 0
 not_added_lyrics = 0
 
-def add(successful):
+def added(successful):
+    ## counting the number of successful and unsuccessful lyrics attachments
+
     global added_lyrics, not_added_lyrics
     if successful:
         added_lyrics += 1
@@ -19,6 +22,8 @@ def add(successful):
 
 
 def get_lyrics(title, artist):
+    ## searching for the lyrics on genius using the title and artist name 
+
     if manual_search:       # manual_search is a global variable, so... not cool
         print(title, '-', artist)
         lyrics = genius.lyrics(input("  url: "))
@@ -27,6 +32,7 @@ def get_lyrics(title, artist):
         # lyrics = genius.search_song(song_id=input("  id: "), get_full_info=False).lyrics
     # lyrics = """"""
 
+    ## formatting the lyrics
     lyrics = re.sub("\n*\[", "\n\n[", lyrics)
     lyrics = lyrics.replace('’', "'").strip()
     lyrics = re.sub("[0-9]*EmbedShare URLCopyEmbedCopy$", '', lyrics)
@@ -36,40 +42,44 @@ def get_lyrics(title, artist):
 
 def set_lyrics_mp3(file):
     try:
+        ## loading the mp3 file tags
         tag = eyed3.load(file).tag
-        t = tag._getTitle()
-        aa = tag._getAlbumArtist()
+        t = tag.title
+        aa = tag.album_artist
 
+        ## if there are parentheses in the title, you need to enter the title manually
         if '(' in t or ')' in t:
             print(t, '-', aa)
             t = input("  t: ")
             if not t.strip():
-                t = tag._getTitle()
+                t = tag.title
 
     except Exception:
-        print(" X File Error: ", file)
-        add(False)
+        print(" X File Error:", file)
+        added(False)
         return
 
     try:
+        ## setting the lyrics and saving it
         lyrics = get_lyrics(t, aa)
         tag.lyrics.set(lyrics)
         tag.save(version=eyed3.id3.ID3_V2_4, encoding="utf8")
-        add(True)
-        return
+        added(True)
 
     except Exception:
-        print(" X Lyrics Error: ", file)
-        add(False)
-        return
+        print(" X Lyrics Error:", file)
+        added(False)
 
 
 def set_lyrics_m4a(file):
     try:
+        ## loading the m4a file tags
         tag = MP4(file).tags
         t = tag.get("\xa9nam")
         aa = tag.get("aART")
 
+        ## there could be more than one title or artist found in the tags
+        ## in that case you need to enter them manually
         if len(t) == 1 and len(aa) == 1:
             t = t[0]
             aa = aa[0]
@@ -77,6 +87,8 @@ def set_lyrics_m4a(file):
             print(t, '-', aa)
             t = input("  t: ")
             aa = input("  aa: ")
+        
+        ## if there are parentheses in the title, you need to enter the title manually
         if '(' in t or ')' in t:
             print(t, '-', aa)
             t = input("  t: ")
@@ -84,24 +96,25 @@ def set_lyrics_m4a(file):
                 t = tag.get("\xa9nam")[0]
 
     except Exception:
-        print(" X File Error: ", file)
-        add(False)
-        return
+        print(" X File Error:", file)
+        added(False)
 
     try:
+        ## setting the lyrics and saving it
         lyrics = get_lyrics(t, aa)
         tag["\xa9lyr"] = lyrics
         tag.save(file)
-        add(True)
-        return
+        added(True)
 
     except Exception:
-        print(" X Lyrics Error: ", file)
-        add(False)
+        print(" X Lyrics Error:", file)
+        added(False)
         return
 
 
 def set_lyrics(path, files):
+    ## looping through the files in one folder and setting lyrics for them
+
     print('\n' + '─'*len(path) + '──┐')
     print(' ' + path + ' │', len(files))
     print('─'*len(path) + '──┘')
@@ -118,15 +131,22 @@ def set_lyrics(path, files):
                 set_lyrics_mp3(file)
             elif file[-4:].lower() == ".m4a":
                 set_lyrics_m4a(file)
+            else:
+                print(" X Not supported file format:", file)
+                added(False)
 
+        ## if 'file' is a folder, saving it to call the function on it later
         elif os.path.isdir(file):
             files2.append(file + '/')
 
+    ## calling the function again for the folders in this directory
     for path2 in files2:
         set_lyrics(path2, sorted(os.listdir(path2)))
 
 
 def load_lyrics(file):
+    ## reading the lyrics from a file
+
     try:
         if file[-4:].lower() == ".mp3":
             lyrics = eyed3.load(file).tag.lyrics[0].text
@@ -136,14 +156,15 @@ def load_lyrics(file):
             lyrics = " X Not supported file format"
 
     except Exception:
-        # lyrics = " X No Lyrics: " + file[len(path):]
         lyrics = " X No Lyrics"
-        print(lyrics, file)
 
     return lyrics
 
 
-def show_lyrics(path, files, txt=False):
+def read_lyrics(path, files, txt=False):
+    ## generating a text, containing lyrics of all the files in a directory
+    ## it calls the load_lyrics function for each file
+
     lyrics = ""
     folder = path.split('/')[-2]
     lyrics += f"\n┌─{'─'*len(folder)}─┐\n│ {folder} │ {len(files)}\n└─{'─'*len(folder)}─┘\n"
@@ -159,22 +180,29 @@ def show_lyrics(path, files, txt=False):
             files2.append(file + '/')
     lyrics += f"\n\n{'─'*120}\n"
 
+    ## saving the lyrics text in a file or just print it
     if txt:
         if os.path.isfile("/home/leli/Music/Lyrics/"+folder+" (Lyrics).txt"):       ## WORKS ON MY MACHINE ERROR
-            print(" Lyrics file already exists.")
+            print(" X Lyrics file already exists:", folder)
         else:
             with open("/home/leli/Music/Lyrics/"+folder+" (Lyrics).txt", 'w') as lyricstxt:
                 # path+folder+" (Lyrics).txt"
                 lyricstxt.write(lyrics)
-            print(" Lyrics file generated.")
+            print(" Lyrics file generated:", folder)
     else:
         print(lyrics)
 
+    ## calling the function again for the folders in this directory
+    ## each folder will generate a separate lyrics text
     for path2 in files2:
-        show_lyrics(path2, sorted(os.listdir(path2)), txt)
+        read_lyrics(path2, sorted(os.listdir(path2)), txt)
 
 
 def edit_lyrics(path, files, editor_path):
+    ## writing lyrics in a text file so it can be edited manually
+    ## you need to open the text file at editor_path and edit the lyrics
+    ## after you are done, you can press enter in the terminal to save and move to the next file
+
     files2 = []
     for file in files:
         print(file, end='')
@@ -182,18 +210,26 @@ def edit_lyrics(path, files, editor_path):
         if os.path.isfile(file):
             lyrics = load_lyrics(file)
 
+            ## writing the lyrics in editor_path
             with open(editor_path, 'w') as lyricstxt:
                 lyricstxt.write(lyrics)
+
+            ## waiting until you edit it as you want
             if input(" >< "):
                 break
+
+            ## reading the edited lyrics from editor_path
             with open(editor_path, 'r') as lyricstxt:
                 lyrics = lyricstxt.read().strip()
 
+            ## adding the new lyrics and saving it
+            ## for mp3
             if file[-4:].lower() == ".mp3":
                 tag = eyed3.load(file).tag
                 tag.lyrics.set(lyrics)
                 tag.save(version=eyed3.id3.ID3_V2_4, encoding="utf8")
 
+            ## or m4a
             elif file[-4:].lower() == ".m4a":
                 tag = MP4(file).tags
                 tag["\xa9lyr"] = lyrics
@@ -206,7 +242,7 @@ def edit_lyrics(path, files, editor_path):
         edit_lyrics(path2, sorted(os.listdir(path2)), editor_path)
 
 
-def search_lyrics(path, files):     ## deprecated
+def search_lyrics(path, files):    ## deprecated
     # print('\n', path, '\n')
     files2 = []
     for file in files:
@@ -233,24 +269,30 @@ def search_lyrics(path, files):     ## deprecated
 
 if __name__ == "__main__":
 
+    ## getting the songs and current path
     songs_path = '/' + input("Enter path of your musics folder: ").strip(" \'\"/").replace("\'\\\'\'", '\'') + '/'
     realpath = os.path.realpath(__file__)  ##TODO: its not so efficient, mabye os.getcwd() was good enough
     realpath = realpath[:realpath.rindex('/')]
 
+    ## if the input path is invalid, creating a folder named 'songs' in the current path
+    ## and using it as songs_path
     if not (os.path.isdir(songs_path) and songs_path!="//" and songs_path!="/./"):
         songs_path = realpath + "/songs/"
         if not os.path.isdir(songs_path):
             os.mkdir(songs_path)
 
+    ## gettin the list of files
     files = sorted(os.listdir(songs_path))
     if not files:
         print("\n Songs folder is empty!\n")
         raise SystemExit
 
 
-    sss = input(" 1.Set Lyrics\n 2.Show Lyrics\n 3.Search Lyrics\n 4.Edit Lyrics\n:")
+    sss = input(" 1.Set Lyrics (m)\n 2.Read Lyrics (t)\n 3.Search Lyrics\n 4.Edit Lyrics\n:")
     print('\n')
 
+    ## 1  -> set lyrics automatically
+    ## 1m -> set lyrics by entering urls
     if sss == '1' or sss == '1m':
         manual_search = sss == '1m'
         client_access_token = "JPmYaQ8e0nffrGMNZB-S2-5qxjAlVEb7YNtrGUG58PyCmy4jG5Z9pK2BN6kPZ6kW"
@@ -262,23 +304,28 @@ if __name__ == "__main__":
             genius = lyricsgenius.Genius(client_access_token, skip_non_songs=True, excluded_terms=["(Remix)", "(Live)"])
 
         set_lyrics(songs_path, files)
-        print(f"\n\n\n successfulls: {added_lyrics} \n unsuccessfulls: {not_added_lyrics}")
+        print(f"\n\n\n successfuls: {added_lyrics} \n unsuccessfuls: {not_added_lyrics}")
 
+    ## printing the lyrics of all files in songs_path
     elif sss == '2':
-        show_lyrics(songs_path, files)
+        read_lyrics(songs_path, files)
 
+    ## creating a text file containing all of the lyrics
     elif sss == '2t':
-        show_lyrics(songs_path, files, True)
+        read_lyrics(songs_path, files, True)
 
+    ## searching for a text in lyrics
     elif sss == '3':
         q = input(" q: ").lower()
         # if len(q) < 4:
         #     q = ''
         search_lyrics(songs_path, files)
 
+    ## lyrics editor
     elif sss == '4':
         edit_lyrics(songs_path, files, realpath + "/lyrics_editor.txt")
 
+    ## just for checking
     elif sss == '5':
         print(songs_path)
         print(files)
